f4c8f8600749239e198821fd4e1710f3b1ce485c
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-09-08 13:55:39 -0700

    Add Section 1

--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -23,33 +23,52 @@ console.log(enrolledStudents);
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
+var harry = "Harry Potter must not return to Hogwarts!"
+console.log(harry);
 
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
   // Store the appropriate data types in each.
 // log all three variables to the console.
 
-// IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
-  // Explain.
+var firstName = "Enzo";
+var isHungry = true;
+var numberOfPets = 0;
 
+console.log(firstName);
+console.log(isHungry);
+console.log(numberOfPets);
+
+// IN WORDS:
+// How did you decide to use the data type you did for each of the three variables above?
+  // Explain. Anything having to do with a name is going to be String
+  // starting with 'is' is usually a truthy or falsey
+  // numberOf would have to do with integers
 
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
+// Re-assign the values to the three variables from the previous challenge to different
   // values (but same data type).
 // log all three variables to the console.
+firstName = "Jared";
+isHungry = false;
+numberOfPets = 99;
+
+console.log(firstName);
+console.log(isHungry);
+console.log(numberOfPets);
 
 
 // YOU DO:
 // Using the variables below, log the total number of snacks to the console:
 var healthySnacks = 6;
 var junkFoodSnacks = 8;
+console.log(healthySnacks + junkFoodSnacks);
 
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
--- section1/relection.md
+++ section1/relection.md
@@ -1,19 +1,30 @@
 ## Section 1 Reflection
 
 1. How did the SuperLearner Article resonate with you? What from this list do you already do? Want to start doing or do more of? Is there anything not on this list, that you would add to it?
+ The article was very interesting, I do like to read instructions thoroughly before starting any type of project before actually doing it, so that i already like to do. I want to start viewing learning as a process, I am the type of person to want to reach my end goal as quickly as possible but that way of thinking does not allow for me to soak in as much knowledge.
 
 2. What are the data types you learned about in this section? In your own words, define each.
+String - Are words that are attached to a variable
+Int - Numbers
+Booleans - true or false
 
 3. How would you log the string `"Hello World!"` to the console?
+console.log("Hello World!")
 
 4. What is/are the character(s) you would use to indicate comments in a JavaScript file? What is the purpose of a code comment?
-
+// or /* */
+they allow you to insert comments that wont be included when the code runs
 5. In your own words, what is a variable? How would you explain it to a 5 year old?
+a variable is a descriptive word that could have things assigned to them such as another word/sentence/number/boolean
 
 6. Think of a site or app you use frequently. What are three variables that are probably used? Which data type would each of those variables probably hold?
+Strings,Int,Boolean
 
 7. In your own words, explain what concatenation is.
+it allows for a var with a certain value to be inserted into a string or characters
 
 8. Think of a site or app you use frequently. Where do you think the developers used concatention?
+google   or sites that say "Welcome back ${name}"
 
 9. What questions do you still have about the work we've done so far? (not having a question is not an option)
+When will the other 4 dataTypes be introduced?
